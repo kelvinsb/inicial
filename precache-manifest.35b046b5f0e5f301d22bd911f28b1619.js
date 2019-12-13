f4195e1e110843d0dfd31d4fa4eddac38c538d79
Kelvin <kelvinbr11@gmail.com>
2019-12-13 01:02:41 -0300

    Updates

---
+++ precache-manifest.35b046b5f0e5f301d22bd911f28b1619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a89dc3d699abdd51344ab0ea9bb54ad",
+    "url": "/inicial/index.html"
+  },
+  {
+    "revision": "85ed91949ef56e191311",
+    "url": "/inicial/static/css/2.7560115c.chunk.css"
+  },
+  {
+    "revision": "f71b03c6df3e5fe00919",
+    "url": "/inicial/static/css/main.c6928cde.chunk.css"
+  },
+  {
+    "revision": "85ed91949ef56e191311",
+    "url": "/inicial/static/js/2.9960b5c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/inicial/static/js/2.9960b5c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f71b03c6df3e5fe00919",
+    "url": "/inicial/static/js/main.a3ca597e.chunk.js"
+  },
+  {
+    "revision": "eda1c69ffcdbb829dd91",
+    "url": "/inicial/static/js/runtime-main.75cbf018.js"
+  }
+]);
